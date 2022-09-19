d3971267961692fe3e5152ed72b37e207e4f36da
sanydge <sanydge@gmail.com>
2022-09-19 22:11:08 +0300

    Basics(CSS, A route+link, route wildcard constraints)

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group whichK
 | contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function($slug){
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+
+    if (!file_exists($path)){
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post'=> $post
+    ]);
+})->where('post', '[A-z_\-]+');
