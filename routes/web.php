6883d47e240204d782d726d8c0e93e43e172b6ac
sanydge <sanydge@gmail.com>
2022-09-22 16:57:53 +0300

    integrating the design

--- routes/web.php
+++ routes/web.php
@@ -21,25 +21,29 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories'=>Category::all()
     ]);
-});
+})->name('home');
 
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
-});
+})->name('category');
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory'=> $category,
+        'categories'=>Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories'=>Category::all()
     ]);
 });
